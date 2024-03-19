9ef585516bbd992421f30e7990933dfd5d3c47e5
Tobias Laurin <52038911+sprunq@users.noreply.github.com>
2024-03-19 20:18:48 +0100

    Auto close after successful build

--- build.py
+++ build.py
@@ -1,6 +1,9 @@
 import os
 import shutil
 import subprocess
+import signal
+import platform
+import sys
 
 BUILD_DIR = "os_build"
 RESOURCE_DIR = "res"
@@ -10,11 +13,18 @@ def clean_dir(dir : str):
         shutil.rmtree(dir)
     os.makedirs(dir)
 
+def get_qemu() -> str:
+   if platform.system() == 'Windows':
+        return "qemu-system-i386w"
+   else:
+        return "qemu"
+
 def build():
     clean_dir(BUILD_DIR)
     shutil.copy(f"{RESOURCE_DIR}/bootconf.txt", BUILD_DIR)
     shutil.copy(f"{RESOURCE_DIR}/bts_dsk.bin", BUILD_DIR)
     os.chdir(BUILD_DIR)
+    
     output = subprocess.run(["java", "-jar", "..\\res\\sjc.jar", "..\\src", "-o", "boot"], 
                        capture_output=True, 
                        text=True) 
@@ -25,8 +35,16 @@ def build():
         print("Detected errors during compilation. Aborting build..")
         return
     
-    
-    subprocess.run(["qemu-system-i386w", "-m", "32", "-boot", "a", "-fda", "BOOT_FLP.IMG"])
+    subprocess.run([get_qemu(), "-m", "32", "-boot", "a", "-fda", "BOOT_FLP.IMG"])
+
+    if len(sys.argv) > 1 and sys.argv[1] == "True":
+        # Close the terminal.
+        # I need to do this since I run the script via the IntelliJ run button, which launches a new Console every run and
+        # after too many runs it becomes very slow.
+        if platform.system() != 'Linux':
+            signal.SIGHUP = 1
+
+        os.kill(os.getppid(), signal.SIGHUP)
 
 if __name__ == "__main__":
     build()
