5c05f491fa88a6902cbef094732b77ec3f6c5126
Tobias Laurin <52038911+sprunq@users.noreply.github.com>
2024-03-20 09:02:17 +0100

    Upadte build script and cleanup directory

--- build.py
+++ build.py
@@ -4,9 +4,11 @@ import subprocess
 import signal
 import platform
 import sys
+import argparse
 
 BUILD_DIR = "os_build"
 RESOURCE_DIR = "res"
+START_DIR = os.getcwd()
 
 def clean_dir(dir : str):
     if os.path.exists(dir):
@@ -19,25 +21,38 @@ def get_qemu() -> str:
    else:
         return "qemu"
 
-def build():
+def build(sjc_jar_path_arg : str, cleanup : bool, autoclose : bool):
+    sjc_jar_absolute = os.path.abspath(sjc_jar_path_arg)
+    if not os.path.exists(sjc_jar_absolute):
+        print(f"unable to locate SJC at {sjc_jar_absolute}")
+        exit()
+
     clean_dir(BUILD_DIR)
-    shutil.copy(f"{RESOURCE_DIR}/bootconf.txt", BUILD_DIR)
-    shutil.copy(f"{RESOURCE_DIR}/bts_dsk.bin", BUILD_DIR)
+    shutil.copy("bootconf.txt", BUILD_DIR)
+    shutil.copy("bts_dsk.bin", BUILD_DIR)
     os.chdir(BUILD_DIR)
-    
-    output = subprocess.run(["java", "-jar", "..\\res\\sjc.jar", "..\\src", "-o", "boot"], 
-                       capture_output=True, 
+
+    output = subprocess.run(["java", "-jar", sjc_jar_absolute, "..\\src", "-o", "boot"],
+                       capture_output=True,
                        text=True) 
     
     print(output.stdout)
+    if output.stderr:
+        print(output.stderr)
 
     if not os.path.exists("BOOT_FLP.IMG"):
         print("Detected errors during compilation. Aborting build..")
-        return
+        exit()
     
     subprocess.run([get_qemu(), "-m", "32", "-boot", "a", "-fda", "BOOT_FLP.IMG"])
 
-    if len(sys.argv) > 1 and sys.argv[1] == "True":
+    # Cleanup
+    os.chdir(START_DIR)
+    
+    if cleanup and os.path.exists(BUILD_DIR):
+        shutil.rmtree(BUILD_DIR)
+
+    if autoclose:
         # Close the terminal.
         # I need to do this since I run the script via the IntelliJ run button, which launches a new Console every run and
         # after too many runs it becomes very slow.
@@ -47,4 +62,11 @@ def build():
         os.kill(os.getppid(), signal.SIGHUP)
 
 if __name__ == "__main__":
-    build()
+    parser = argparse.ArgumentParser(prog='ToOS Build tool')
+    parser.add_argument('--sjc', metavar='path_to_jar', type=str, required=True, help='path to the sjc jar file')
+    parser.add_argument('--autoclose', action='store_true', help='auto close terminal after completion')
+    parser.add_argument('--cleanup', action='store_true', help='delete build directory after completion')
+    
+    args = parser.parse_args()
+    
+    build(args.sjc, args.cleanup, args.autoclose)
