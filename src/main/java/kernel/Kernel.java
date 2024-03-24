c18cf8ae39c5e4c2e4d718ef55b05dbaecb3a2d1
Tobias Laurin <52038911+sprunq@users.noreply.github.com>
2024-03-25 00:11:04 +0100

    refactor email bug submission

--- src/main/java/kernel/Kernel.java
+++ src/main/java/kernel/Kernel.java
@@ -2,25 +2,27 @@ package kernel;
 
 import kernel.display.textmode.TmColor;
 import kernel.display.textmode.TmWriter;
+import kernel.memory.MemoryManager;
 
 public class Kernel {
     public static TmWriter out;
 
     public static void main() {
         out = new TmWriter();
-        main_code();
+        // entkommentieren, um Alloc zu testen
+        // show_alloc_functionality();
+        test_memory_limit();
     }
 
-    private static void main_code() {
+    private static void test_memory_limit() {
         out.clearScreen();
-
         int i = 0;
         while (true) {
             i++;
             TestAllocA e = new TestAllocA(42, 43, 44, false, "_");
 
             if (e.a != 42) {
-                out.print("Error in object at ");
+                out.print("Error in object at 0x");
                 out.print(MAGIC.cast2Ref(e), 16);
                 out.println(": Values are not correct.");
                 break;
@@ -32,6 +34,80 @@ public class Kernel {
         }
 
         while (true) {
+        }
+
+    }
+
+    private static void show_alloc_functionality() {
+        out.clearScreen();
+
+        TestAllocA a = new TestAllocA(5, 7, 9, true, "hello from a");
+        TestAllocB b = new TestAllocB(a);
+
+        int[] addrs = new int[6];
+        addrs[0] = MAGIC.cast2Ref(a);
+        addrs[1] = MAGIC.cast2Ref(b);
+        addrs[3] = MAGIC.cast2Ref(addrs);
+        addrs[4] = MAGIC.cast2Ref(out);
+        addrs[5] = MAGIC.cast2Ref(out.brush);
+
+        Object obj = MemoryManager.getFirstHeapObj();
+        int foundObjects = 0;
+        while (obj != null) {
+            int addr = MAGIC.cast2Ref(obj);
+
+            boolean found = false;
+            for (int i = 0; i < addrs.length; i++) {
+                if (addrs[i] == addr) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                out.brush.setFg(TmColor.LIGHT_GREEN);
+            } else {
+                out.brush.setFg(TmColor.LIGHT_RED);
+            }
+
+            out.print("0_");
+            out.print(addr, 10);
+            out.print(": ");
+            out.print("object(relocEntries=");
+            out.print(obj._r_relocEntries, 10);
+            out.print(", scalarSize=");
+            out.print(obj._r_scalarSize, 10);
+            out.println(")");
+            obj = obj._r_next;
+            foundObjects++;
+            // sleep();
+        }
+
+        out.brush.setFg(TmColor.WHITE);
+        out.print("Found ");
+        out.print(foundObjects, 10);
+        out.println(" objects.");
+
+        out.print("Consumed memory: ");
+        out.print(MemoryManager.getConsumedMemory(), 10);
+        out.println("b");
+
+        // Show that the objects are not overwritten
+        TestAllocA c = new TestAllocA(11, 13, 15, false, "hello from c");
+        TestAllocB d = new TestAllocB(c);
+
+        b.print();
+        out.println();
+        d.print();
+
+        out.println();
+
+        while (true) {
+        }
+    }
+
+    private static void sleep() {
+        for (int i = 0; i < 3000000; i++) {
         }
     }
 
