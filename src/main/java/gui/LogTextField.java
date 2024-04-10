fe5ac1682634efea7a87ba962ac5131fb362fffb
sprunq <52038911+sprunq@users.noreply.github.com>
2024-04-11 01:35:13 +0200

    LogTextField color constants

--- src/main/java/gui/LogTextField.java
+++ src/main/java/gui/LogTextField.java
@@ -6,6 +6,14 @@ import kernel.display.video.VM13;
 import kernel.display.video.font.AFont;
 
 public class LogTextField extends TextField {
+    private final byte COL_FALLBACK;
+    private final byte COL_FATAL;
+    private final byte COL_ERROR;
+    private final byte COL_WARNING;
+    private final byte COL_INFO;
+    private final byte COL_TRACE;
+    private final byte COL_WHITE;
+
     public LogTextField(
             int x,
             int y,
@@ -17,6 +25,13 @@ public class LogTextField extends TextField {
             int lineSpacing,
             byte backGroundColor) {
         super(x, y, width, height, border, charSpacing, lineSpacing, backGroundColor, VM13.frgb(1.0, 1.0, 1.0), font);
+        COL_FALLBACK = VM13.frgb(1.0, 0.5, 1.0);
+        COL_FATAL = VM13.frgb(0.6, 0.0, 0.0);
+        COL_ERROR = VM13.frgb(0.9, 0.1, 0.1);
+        COL_WARNING = VM13.frgb(1.0, 0.7, 0.0);
+        COL_INFO = VM13.frgb(0.5, 0.7, 1.0);
+        COL_TRACE = VM13.frgb(0.4, 0.8, 0.4);
+        COL_WHITE = VM13.frgb(1.0, 1.0, 1.0);
     }
 
     public void draw() {
@@ -26,28 +41,28 @@ public class LogTextField extends TextField {
             LogEntry log = Logger.getChronologicalLog(i);
             if (log != null) {
                 String msg = log.getMessage();
-                String cat = log.getCategory();
                 if (msg.length() != 0) {
+                    String cat = log.getCategory();
                     byte level = (byte) log.getPriority();
                     byte color = 0;
                     switch (level) {
                         case Logger.TRACE:
-                            color = VM13.frgb(0.4, 0.8, 0.4);
+                            color = COL_TRACE;
                             break;
                         case Logger.INFO:
-                            color = VM13.frgb(0.5, 0.7, 1.0);
+                            color = COL_INFO;
                             break;
                         case Logger.WARNING:
-                            color = VM13.frgb(1.0, 0.7, 0.0);
+                            color = COL_WARNING;
                             break;
                         case Logger.ERROR:
-                            color = VM13.frgb(0.9, 0.1, 0.1);
+                            color = COL_ERROR;
                             break;
                         case Logger.FATAL:
-                            color = VM13.frgb(0.6, 0.0, 0.0);
+                            color = COL_FATAL;
                             break;
                         default:
-                            color = VM13.frgb(1.0, 0.5, 1.0);
+                            color = COL_FALLBACK;
                             break;
                     }
                     setBrushColor(color);
@@ -61,7 +76,7 @@ public class LogTextField extends TextField {
         }
         setCursor(0, 0);
         clearLine(0);
-        setBrushColor(VM13.frgb(1.0, 1.0, 1.0));
+        setBrushColor(COL_WHITE);
         addString("Log Entries");
         newLine();
         super.draw();
