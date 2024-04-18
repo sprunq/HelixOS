291070716f859a7e84b33ab86351ecf082b76096
sprunq <52038911+sprunq@users.noreply.github.com>
2024-04-18 20:33:09 +0200

    remove sse3 function

--- src/main/java/kernel/Kernel.java
+++ src/main/java/kernel/Kernel.java
@@ -16,7 +16,6 @@ import kernel.hardware.Timer;
 import kernel.hardware.keyboard.KeyboardController;
 import kernel.hardware.keyboard.layout.QWERTZ;
 import kernel.interrupt.IDT;
-import kernel.memory.Memory;
 import kernel.memory.MemoryManager;
 import util.logging.Logger;
 import util.vector.VectorVesaMode;
@@ -25,7 +24,6 @@ public class Kernel {
     public static ADisplay Display;
 
     public static void main() {
-        Memory.enableSSE3();
         MemoryManager.initialize();
         Logger.initialize(Logger.TRACE, 200);
         KeyboardController.initialize(QWERTZ.Instance);
